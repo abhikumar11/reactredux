4fdbaaf5d360e8d727718e6ce0ac21a9b0f25ed3
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-08-13 13:11:47 +0530

    initial commit

---
+++ src/components/Product.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import laptop from "../assets/laptop.png";
+import mobile from "../assets/mobile.png";
+import watch from "../assets/watch.png";
+const Product = () => {
+    const productList=[
+        {
+            id:1,name:"laptop",price:33000,image:laptop
+        }, 
+        {
+            id:2,name:"mobile",price:6500,image:mobile
+        },
+         {
+            id:3,name:"watch",price:1300,image:watch
+        }
+    ]
+  return (
+    <div>
+        <h1>Products</h1>
+        <div style={{display:"flex",justifyContent:"space-around"}}>
+            {productList.map((item)=>(
+                <div style={{border:"1px solid black",padding:"16px"}}>
+                    <img src={item.image} height={"200px"}/>
+                    <h2>{item.name}</h2>
+                    <p>&#8377;{item.price}</p>
+                    <button>Add to Cart</button><br/>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Product
